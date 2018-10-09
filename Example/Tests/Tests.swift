e52f9031e6136aab819500c423f359306e88e1d5
jalil.kennedy <jalilkennedy@gmail.com>
2018-10-09 17:10:16 -0400

    Add ReactiveARSKView

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -1,5 +1,5 @@
 import XCTest
-@testable import ReactiveARKit
+import ReactiveARKit
 
 class Tests: XCTestCase {
     
---
+++ ReactiveARSKView.swift
@@ -0,0 +1,8 @@
+//
+//  ReactiveARSKView.swift
+//  Pods
+//
+//  Created by jalil.kennedy on 10/9/18.
+//
+
+import Foundation
