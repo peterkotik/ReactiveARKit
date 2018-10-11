f6506a5074925faa31ad2311cdb1366d3ae6310d
jalil.kennedy <jalilkennedy@gmail.com>
2018-10-11 15:46:53 -0400

    Add PublishSubjects for ARSessionDelegate methods

--- Example/ReactiveARKit/ViewController.swift
+++ Example/ReactiveARKit/ViewController.swift
@@ -7,12 +7,60 @@
 //
 
 import UIKit
+import ARKit
+import RxSwift
+import ReactiveARKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var sceneView: ReactiveARSKView!
+    
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        guard let scene = SKScene(fileNamed: "Scene") else { fatalError("Failed to load initial SKScene") }
+        
+        sceneView.presentScene(scene)
+        
+        #if debug
+        sceneView.showsFPS = true
+        sceneView.showsNodeCount = true
+        #endif
+        
+        sceneView.sessionStarted.subscribe {
+            print("sessionStarted: \($0)")
+        }.disposed(by: disposeBag)
+        
+        sceneView.didAddAnchors.subscribe {
+            print("didAddAnchors: \($0)")
+        }.disposed(by: disposeBag)
+        
+        sceneView.didUpdateAnchors.subscribe { [weak self] in
+            print("didUpdateAnchors: \($0)")
+            guard
+                let self = self,
+                let tuple = $0.element,
+                let anchor = tuple.1.first
+                else {
+                print("Could not cast tuple to non optional tuple in didUpdateAnchors")
+                return
+            }
+
+            self.sceneView.session.remove(anchor: anchor)
+        }.disposed(by: disposeBag)
+        
+        sceneView.didRemoveAnchors.subscribe {
+            print("didRemoveAnchors: \($0)")
+        }.disposed(by: disposeBag)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+        
+        sceneView.session.run(configuration)
     }
 
     override func didReceiveMemoryWarning() {
